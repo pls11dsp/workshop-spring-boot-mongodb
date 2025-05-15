1b0455d2273fedda6f18450b31fc6aaa1b3460fb
Pedro Luis Somera <pls11@dspcontroles.com.br>
2025-05-14 21:40:42 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/pls11dspcontroles/workshopmongo/config/Instantiation.java
+++ src/main/java/com/pls11dspcontroles/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.pls11dspcontroles.workshopmongo.domain.Post;
 import com.pls11dspcontroles.workshopmongo.domain.User;
+import com.pls11dspcontroles.workshopmongo.dto.AuthorDTO;
 import com.pls11dspcontroles.workshopmongo.repositories.PostRepository;
 import com.pls11dspcontroles.workshopmongo.repositories.UserRepository;
 
@@ -35,10 +36,10 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
-		
 		userRepoistory.saveAll(Arrays.asList(maria, alex, bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+				
 		postRepoistory.saveAll(Arrays.asList(post1, post2));
 	}
 
